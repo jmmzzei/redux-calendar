de1efd4868022e8f4466b9274fce03ff2b673dbd
jmm <josemmazzei@gmail.com>
2020-04-25 22:49:55 -0300

    Style navbar and fix overflow-x bug

--- src/components/NavbarStyled.js
+++ src/components/NavbarStyled.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 export const NavbarStyled = styled.nav`
-  height: 80px;
+  height: 50px;
   bakground: #f1f1f1;
   width: 100%;
+  font-size: 2rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin: 0;
-  padding: 20px;
+  color: white;
+  padding: 20px 0;
   display: flex;
   algin-items: center;
   justify-content: center;
+  background: #000045;
 `;
