886605e5447230b752a8b8d5d3707d8420f5c344
jmm <josemmazzei@gmail.com>
2020-05-14 13:00:01 -0300

    Modify Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { NavbarStyled } from "./NavbarStyled";
+import React from "react"
+import { NavbarStyled } from "./NavbarStyled"
 
-export const Navbar = ({ month }) => {
-    return <NavbarStyled>{month || '2020 Calendar'}</NavbarStyled>;
-};
+export const Navbar = ({ month }) => (
+  <NavbarStyled>{month || "2020 Calendar"}</NavbarStyled>
+)
--- src/components/NavbarStyled.js
+++ src/components/NavbarStyled.js
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const NavbarStyled = styled.nav`
-  height: 50px;
-  bakground: #f1f1f1;
-  width: 100%;
-  font-size: 2rem;
-  font-weight: bold;
+    color: var(--light);
+  background: var(--dark);
+  user-select:none;
+
   display: flex;
   align-items: center;
   justify-content: center;
+  
+  height: 5rem;
   margin: 0;
-  color: white;
-  padding: 20px 0;
-  display: flex;
-  algin-items: center;
-  justify-content: center;
-  background: #000045;
+  padding: 4rem 0;
+ 
+  font-size: 3rem;
+  font-weight: bold;
+
 `;
