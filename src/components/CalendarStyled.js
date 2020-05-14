1c38693473212df8d9cd82f64fb29a681f9d8cee
jmm <josemmazzei@gmail.com>
2020-05-14 16:20:43 -0300

    Add grid-gap to CalendarStyled

--- src/components/CalendarStyled.js
+++ src/components/CalendarStyled.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
 export const CalendarStyled = styled.section`
-  background: white;
+  height: calc(100vh - 5rem - 5rem); 
   margin: 0;
-  padding: 5px;
+  padding: 0.5rem;
   display: grid;
   grid-template-columns: repeat(${props => props.month ? 6 : 4}, 1fr);
   grid-template-rows: repeat(${props => props.month ? 5 : 3}, 1fr);
+  grid-gap: ${props => props.month ? 1 : 5}rem;
 `;
