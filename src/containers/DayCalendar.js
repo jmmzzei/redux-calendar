72efed5d262fff85bf340209b395fcd348bc8dea
jmm <josemmazzei@gmail.com>
2020-05-16 13:47:23 -0300

    Add items prop to CalendarStyled

--- src/containers/DayCalendar.js
+++ src/containers/DayCalendar.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { CalendarStyled } from '../components/CalendarStyled'
-import { Navbar } from '../components/Navbar'
-import DayItem from '../components/DayItem'
+import React from "react"
+import { connect } from "react-redux"
+import { CalendarStyled } from "../components/CalendarStyled"
+import { Navbar } from "../components/Navbar"
+import DayItem from "../components/DayItem"
 
 const DayCalendar = ({ match, months }) => {
-    let month = match.params.month
-    return (
-        <>
-            <Navbar month={month} />
-            <CalendarStyled month>
-                {(() => {
-                    let arr = []
-                    for (let i = 1; i <= months[month].days; i++) {
-                        arr.push(<DayItem key={i} day={i} month={month} activities={months[month].activities} />)
-                    }
-                    return arr
-                }
-                )()}
-
-            </CalendarStyled>
-        </>
-    )
+  let month = match.params.month
+  return (
+    <>
+      <Navbar month={month} />
+      <CalendarStyled month items={months[month].days}>
+        {(() => {
+          let arr = []
+          for (let i = 1; i <= months[month].days; i++) {
+            arr.push(
+              <DayItem
+                key={i}
+                day={i}
+                month={month}
+                activities={months[month].activities}
+              />,
+            )
+          }
+          return arr
+        })()}
+      </CalendarStyled>
+    </>
+  )
 }
 
-const mapStateToProps = (state) => (
-    { months: { ...state } }
-)
+const mapStateToProps = state => ({ months: { ...state } })
 
 export default connect(mapStateToProps)(DayCalendar)
