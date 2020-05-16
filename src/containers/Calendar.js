bdb62529d8da86ad7aff88772d161e3af2b1d400
jmm <josemmazzei@gmail.com>
2020-05-16 13:35:10 -0300

    Add id to MonthItems

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -1,20 +1,17 @@
 import React from "react"
-import { CalendarStyled } from "../components/CalendarStyled";
-import { connect } from "react-redux";
-import { MonthItem } from "../components/MonthItem";
+import { CalendarStyled } from "../components/CalendarStyled"
+import { connect } from "react-redux"
+import { MonthItem } from "../components/MonthItem"
 
-const Calendar = ({ months }) => {
+const Calendar = ({ months }) => (
+  <CalendarStyled items={months.length}>
+    {months &&
+      months.map((e, i) => <MonthItem id={"id" + (i + 1)} key={i} month={e} />)}
+  </CalendarStyled>
+)
 
-    return (
-
-        <CalendarStyled>
-            {months &&
-                months.map((e, i) => <MonthItem key={i} month={e} />)}
-        </CalendarStyled>
-    )
+const mapStateToProps = state => {
+  return { months: [...Object.keys(state)] }
 }
-const mapStateToProps = (state) => {
-    return { months: [...Object.keys(state)] }
-};
 
 export default connect(mapStateToProps)(Calendar)
