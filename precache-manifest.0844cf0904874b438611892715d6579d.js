6832b829943ca066a909cad9188b214c1571af20
jmm <josemmazzei@gmail.com>
2020-05-17 17:29:42 -0300

    Updates

---
+++ precache-manifest.0844cf0904874b438611892715d6579d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a59320529f3aa96670639d81997661",
+    "url": "/redux-calendar/index.html"
+  },
+  {
+    "revision": "f81ef78ea58f93f1d9c2",
+    "url": "/redux-calendar/static/css/main.2e764aee.chunk.css"
+  },
+  {
+    "revision": "52553cf86a8778d334d6",
+    "url": "/redux-calendar/static/js/2.956cb1b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-calendar/static/js/2.956cb1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81ef78ea58f93f1d9c2",
+    "url": "/redux-calendar/static/js/main.1abe1dde.chunk.js"
+  },
+  {
+    "revision": "d1f8710967c515706e46",
+    "url": "/redux-calendar/static/js/runtime-main.2613138c.js"
+  }
+]);
