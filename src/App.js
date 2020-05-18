98ba0fb0b7bfce23cd9cb6c2aa7935d2090f1d53
jmm <josemmazzei@gmail.com>
2020-05-17 21:19:33 -0300

    Switch from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import React, { useEffect, useRef } from "react";
-import "./App.css";
-import { Navbar } from "./components/Navbar";
-import Calendar from "./containers/Calendar";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import DayCalendar from './containers/DayCalendar'
-import { connect } from 'react-redux'
-import { saveStateInLocalStorage, loadStateFromLocalStorage } from './helpers/localStorageManager'
-import { syncStoreAC } from './actions/syncStoreAC'
+import React, { useEffect, useRef } from "react"
+import "./App.css"
+import { Navbar } from "./components/Navbar"
+import Calendar from "./containers/Calendar"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import DayCalendar from "./containers/DayCalendar"
+import { connect } from "react-redux"
+import {
+  saveStateInLocalStorage,
+  loadStateFromLocalStorage,
+} from "./helpers/localStorageManager"
+import { syncStoreAC } from "./actions/syncStoreAC"
 
 function App(props) {
-    let { dispatch, ...months } = props
+  let { dispatch, ...months } = props
 
-    useEffect(() => {
-        if (loadStateFromLocalStorage()) {
-            props.dispatch(syncStoreAC(loadStateFromLocalStorage()))
-        } else {
-            saveStateInLocalStorage(months)
-        }
-    }, [])
+  useEffect(() => {
+    if (loadStateFromLocalStorage()) {
+      props.dispatch(syncStoreAC(loadStateFromLocalStorage()))
+    } else {
+      saveStateInLocalStorage(months)
+    }
+  }, [])
 
-    useEffect(() => {
-        saveStateInLocalStorage(months)
-    }, [months])
+  useEffect(() => {
+    saveStateInLocalStorage(months)
+  }, [months])
 
-    return (
-        <Router>
-            <Switch>
-                <Route path='/:month'
-                    component={DayCalendar} />
-                <Route path='/'>
-                    <Navbar />
-                    <Calendar />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router basename="/redux-calendar">
+      <Switch>
+        <Route path="/:month" component={DayCalendar} />
+        <Route path="/">
+          <Navbar />
+          <Calendar />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-const mapStateToProps = (state) => {
-    return ({ ...state })
+const mapStateToProps = state => {
+  return { ...state }
 }
 
 export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import store from "./store/store";
-import { Provider } from "react-redux";
-import { initialState } from "./state/initialState";
-import { BrowserRouter } from 'react-router-dom'
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import store from "./store/store"
+import { Provider } from "react-redux"
+import { initialState } from "./state/initialState"
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store(initialState)}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+  <React.StrictMode>
+    <HashRouter>
+      <Provider store={store(initialState)}>
+        <App />
+      </Provider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root"),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
